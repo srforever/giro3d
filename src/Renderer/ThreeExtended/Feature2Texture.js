fdb7a23ecfe231e5e375518811910b27880f1389
Augustin Trancart <augustin.trancart@gmail.com>
2018-10-31 18:46:36 +0100

    Support lineCap in Feature2Texture

--- src/Renderer/ThreeExtended/Feature2Texture.js
+++ src/Renderer/ThreeExtended/Feature2Texture.js
@@ -45,6 +45,7 @@ function _drawPolygon(ctx, vertices, indices, origin, scale, properties, style) 
         ctx.strokeStyle = style.stroke || properties.stroke;
         ctx.lineWidth = style.strokeWidth || properties['stroke-width'] || 2.0;
         ctx.globalAlpha = style.strokeOpacity || properties['stroke-opacity'] || 1.0;
+        ctx.lineCap = style.strokeCap || properties['stroke-cap'] || 'butt';
         ctx.stroke();
     }
 
