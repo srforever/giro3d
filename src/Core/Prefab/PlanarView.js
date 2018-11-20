ce51d4d314b2f7f67cc2353e62b2026d75f44764
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2018-11-20 16:35:48 +0100

    wip: when using a 3857 extent, align the geometry on the 3857 tilegrid
    Since we only display one texture per tile, we need to align our
    tile on the texture tilegrid.

--- src/Core/Prefab/PlanarView.js
+++ src/Core/Prefab/PlanarView.js
@@ -5,11 +5,65 @@ import { MAIN_LOOP_EVENTS } from '../MainLoop';
 import RendererConstant from '../../Renderer/RendererConstant';
 import { GeometryLayer } from '../Layer/Layer';
 import PlanarTileBuilder from './Planar/PlanarTileBuilder';
+import Extent from '../Geographic/Extent';
+
+function compute3857Extent(tileExtent) {
+    const extent = new Extent('EPSG:3857',
+        -20037508.342789244, 20037508.342789244,
+        -20037508.342789255, 20037508.342789244);
+    const c = tileExtent.center();
+    const layerDimension = extent.dimensions();
+
+    // Each level has 2^n * 2^n tiles...
+    // ... so we count how many tiles of the same width as tile we can fit in the layer
+    const tileCount = Math.min(
+        Math.round(layerDimension.x / tileExtent.dimensions().x),
+        Math.round(layerDimension.y / tileExtent.dimensions().y));
+    // ... 2^zoom = tilecount => zoom = log2(tilecount)
+    const zoom = Math.floor(Math.log2(tileCount));
+    const realTileCount = Math.pow(2, zoom);
+    const tileSize = {
+        x: layerDimension.x / realTileCount,
+        y: layerDimension.y / realTileCount,
+    };
+
+    // Now that we have computed zoom, we can deduce x and y (or row / column)
+    const x = (c.x() - extent.west()) / layerDimension.x;
+    const y = (extent.north() - c.y()) / layerDimension.y;
+
+    const left = extent.west() + Math.floor(x * realTileCount) * tileSize.x;
+    const top = extent.north() - Math.floor(y * realTileCount) * tileSize.y;
+    return new Extent('EPSG:3857',
+        left, left + tileSize.x,
+        top - tileSize.y, top);
+}
 
 export function createPlanarLayer(id, extent, options) {
     const tileLayer = new GeometryLayer(id, options.object3d || new THREE.Group());
     tileLayer.extent = extent;
-    tileLayer.schemeTile = [extent];
+
+    if (extent.crs() == 'EPSG:3857') {
+        // align quadtree on EPSG:3857 full extent
+        const aligned = compute3857Extent(extent);
+        if (!extent.isInside(aligned)) {
+            throw new Error('oops');
+        }
+        if (aligned.isInside(extent)) {
+            tileLayer.schemeTile = [extent];
+        } else {
+            tileLayer.schemeTile = [];
+            const quads = aligned.quadtreeSplit();
+            for (const quad of quads) {
+                const inter = quad.intersect(extent);
+                if (inter.dimensions().x > 0) {
+                    tileLayer.schemeTile.push(inter);
+                }
+            }
+        }
+    } else {
+        tileLayer.schemeTile = [extent];
+    }
+    tileLayer.maxSubdivisionLevel = options.maxSubdivisionLevel;
 
     tileLayer.postUpdate = (context, layer) => {
         for (const r of layer.level0Nodes) {
