941f6cd9e8c3b4dcb16e77606fc89d8db69921a1
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2018-11-29 15:32:01 +0100

    fixup! wip: when using a 3857 extent, align the geometry on the 3857 tilegrid
    f

--- src/Core/Prefab/PlanarView.js
+++ src/Core/Prefab/PlanarView.js
@@ -6,36 +6,59 @@ import RendererConstant from '../../Renderer/RendererConstant';
 import { GeometryLayer } from '../Layer/Layer';
 import PlanarTileBuilder from './Planar/PlanarTileBuilder';
 import Extent from '../Geographic/Extent';
+import Coordinates from '../Geographic/Coordinates';
 
+function findCellWith(x, y, layerDimension, tileCount) {
+    const tx = tileCount * x / layerDimension.x;
+    const ty = tileCount * y / layerDimension.y;
+    return { x: Math.round(tx), y: Math.round(ty) }
+ }
+
+
+// return the 3857 tile that fully contains the given extent
 function compute3857Extent(tileExtent) {
     const extent = new Extent('EPSG:3857',
         -20037508.342789244, 20037508.342789244,
         -20037508.342789244, 20037508.342789244);
-    const c = tileExtent.center();
     const layerDimension = extent.dimensions();
 
     // Each level has 2^n * 2^n tiles...
     // ... so we count how many tiles of the same width as tile we can fit in the layer
     const tileCount = Math.min(
-        Math.round(layerDimension.x / tileExtent.dimensions().x),
-        Math.round(layerDimension.y / tileExtent.dimensions().y));
+        Math.floor(layerDimension.x / tileExtent.dimensions().x),
+        Math.floor(layerDimension.y / tileExtent.dimensions().y));
     // ... 2^zoom = tilecount => zoom = log2(tilecount)
-    const zoom = Math.floor(Math.log2(tileCount));
-    const realTileCount = Math.pow(2, zoom);
-    const tileSize = {
-        x: layerDimension.x / realTileCount,
-        y: layerDimension.y / realTileCount,
-    };
+    let zoom = Math.floor(Math.log2(tileCount));
 
-    // Now that we have computed zoom, we can deduce x and y (or row / column)
-    const x = (c.x() - extent.west()) / layerDimension.x;
-    const y = (extent.north() - c.y()) / layerDimension.y;
+    const tl = new Coordinates('EPSG:3857', tileExtent.west(), tileExtent.north());
+    const br = new Coordinates('EPSG:3857', tileExtent.east(), tileExtent.south());
+    const center = tileExtent.center();
+    while (zoom >= 0) {
+        const realTileCount = Math.pow(2, zoom);
 
-    const left = extent.west() + Math.floor(x * realTileCount) * tileSize.x;
-    const top = extent.north() - Math.floor(y * realTileCount) * tileSize.y;
-    return new Extent('EPSG:3857',
-        left, left + tileSize.x,
-        top - tileSize.y, top);
+        // compute tile that contains the center
+        const candidate = findCellWith(
+            center.x() - extent.west(), extent.north() - center.y(),
+            layerDimension, realTileCount);
+
+        const tileSize = {
+            x: layerDimension.x / realTileCount,
+            y: layerDimension.y / realTileCount,
+        };
+
+        const left = extent.west() + candidate.x * tileSize.x;
+        const top = extent.north() - candidate.y * tileSize.y;
+
+        const ex = new Extent('EPSG:3857',
+            left, left + tileSize.x,
+            top - tileSize.y, top);
+
+        if (ex.isPointInside(tl) &&
+            ex.isPointInside(br)) {
+            return ex;
+        }
+        zoom --;
+    }
 }
 
 export function createPlanarLayer(id, extent, options) {
