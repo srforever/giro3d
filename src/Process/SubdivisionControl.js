391612cd2bfb60e4fcdf40c92c4a3707250626b6
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2018-11-29 22:16:27 +0100

    Revert "Remove subdivisioncontrol"
    This reverts commit 9761b629013bb9879125f91ec60405d8a700de46.

---
+++ src/Process/SubdivisionControl.js
@@ -0,0 +1,40 @@
+export default {
+    preUpdate: (context, layer) => {
+        context.colorLayers = context.view.getLayers(
+            (l, a) => a && a.id == layer.id && l.type == 'color');
+        context.elevationLayers = context.view.getLayers(
+            (l, a) => a && a.id == layer.id && l.type == 'elevation');
+    },
+
+    hasEnoughTexturesToSubdivide: (context, layer, node) => {
+        // TODO: call texture inheritance from subdivision
+        // return true;
+
+        // Prevent subdivision if node is covered by at least one elevation layer
+        // and if node doesn't have a elevation texture yet.
+        for (const e of context.elevationLayers) {
+            if (!e.frozen && e.ready && e.tileInsideLimit(node, e) && !node.material.isLayerTextureLoaded({ type: 'elevation' })) {
+                // no stop subdivision in the case of a loading error
+                if (node.layerUpdateState[e.id] && node.layerUpdateState[e.id].inError()) {
+                    continue;
+                }
+                return false;
+            }
+        }
+
+        // Prevent subdivision if missing color texture
+        for (const c of context.colorLayers) {
+            if (c.frozen || !c.visible || !c.ready) {
+                continue;
+            }
+            // no stop subdivision in the case of a loading error
+            if (node.layerUpdateState[c.id] && node.layerUpdateState[c.id].inError()) {
+                continue;
+            }
+            if (c.tileInsideLimit(node, c) && !node.material.isLayerTextureLoaded(c)) {
+                return false;
+            }
+        }
+        return true;
+    },
+};
--- src/Process/TiledNodeProcessing.js
+++ src/Process/TiledNodeProcessing.js
@@ -2,6 +2,7 @@ import { Vector3 } from 'three';
 import CancelledCommandException from '../Core/Scheduler/CancelledCommandException';
 import ObjectRemovalHelper from './ObjectRemovalHelper';
 import ScreenSpaceError from '../Core/ScreenSpaceError';
+import SubdivisionControl from './SubdivisionControl';
 
 function requestNewTile(view, scheduler, geometryLayer, extent, parent, level) {
     const command = {
@@ -94,14 +95,11 @@ function testTileSSE(tile, sse, maxLevel) {
 }
 
 function preUpdate(context, layer, changeSources) {
+    SubdivisionControl.preUpdate(context, layer);
+
     if (__DEBUG__) {
         layer._latestUpdateStartingLevel = 0;
     }
-
-    context.colorLayers = context.view.getLayers(
-        (l, a) => a && a.id == layer.id && l.type == 'color');
-    context.elevationLayers = context.view.getLayers(
-        (l, a) => a && a.id == layer.id && l.type == 'elevation');
 
     if (changeSources.has(undefined) || changeSources.size == 0) {
         return layer.level0Nodes;
@@ -188,7 +186,9 @@ function update(context, layer, node) {
                 node.sse = sse; // DEBUG
             }
 
-            if (testTileSSE(node, sse, layer.maxSubdivisionLevel || -1)) {
+            if (node.pendingSubdivision ||
+                (testTileSSE(node, sse, layer.maxSubdivisionLevel || -1) &&
+                    SubdivisionControl.hasEnoughTexturesToSubdivide(context, layer, node))) {
                 subdivideNode(context, layer, node);
                 // display iff children aren't ready
                 node.setDisplayed(node.pendingSubdivision);
