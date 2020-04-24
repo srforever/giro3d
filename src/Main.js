020285bb7f5b207e69f2ba872bd2382b71cd0bf4
Augustin Trancart <augustin.trancart@oslandia.com>
2020-04-24 11:19:31 +0200

    Remove PointDnD

--- src/Main.js
+++ src/Main.js
@@ -26,7 +26,6 @@ export { default as Picking } from './Core/Picking';
 export { default as OBB } from './Renderer/ThreeExtended/OBB';
 
 export { default as DeformationChain } from './DeformationChain';
-export { default as PointDnD } from './PointDnD';
 export { default as HoverHelper } from './HoverHelper';
 export { default as OrthoCameraControls } from './OrthoCameraControls';
 
