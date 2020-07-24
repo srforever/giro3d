bc10e7f449bbfd0ed54a04072537cc6104b3214a
Augustin Trancart <augustin.trancart@oslandia.com>
2020-07-24 15:08:10 +0200

    lint: allow for...of

--- .eslintrc.js
+++ .eslintrc.js
@@ -59,7 +59,23 @@ module.exports = {
         // we make heavy use of for loop, and continue is very handy when used correctly
         'no-continue': 'off',
         'no-param-reassign': 'off', // we use param reassign too much with targets
-        'no-use-before-define': ["error", "nofunc"]
+        'no-use-before-define': ["error", "nofunc"],
+        // same as airbnb, but allow for..of, because our babel config doesn't import generators
+        'no-restricted-syntax': [
+          'error',
+          {
+            selector: 'ForInStatement',
+            message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+          },
+          {
+            selector: 'LabeledStatement',
+            message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+          },
+          {
+            selector: 'WithStatement',
+            message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+          },
+        ],
     },
     "globals": {
         "__DEBUG__": false
