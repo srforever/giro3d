4c713e497fbadf2edb94292ba79524b707d7b3e3
Augustin Trancart <augustin.trancart@oslandia.com>
2020-09-18 11:09:48 +0200

    test: fix 3dtiles.test.js

--- test/unit/3dtiles.test.js
+++ test/unit/3dtiles.test.js
@@ -1,10 +1,9 @@
-/* global describe, it */
+/* global describe, it, expect */
 import proj4 from 'proj4';
-import assert from 'assert';
 import { Matrix4, Object3D } from 'three';
 import Camera from '../../src/Renderer/Camera.js';
 import Coordinates from '../../src/Core/Geographic/Coordinates.js';
-import { computeNodeSSE, _testing as tested } from '../../src/Process/3dTilesProcessing.js';
+import { _testing as tested } from '../../src/Process/3dTilesProcessing.js';
 import { $3dTilesIndex, configureTile } from '../../src/Provider/3dTilesProvider.js';
 
 function tilesetWithBox(transformMatrix) {
@@ -46,7 +45,6 @@ describe('Distance computation using boundingVolume.box', () => {
     const camera = new Camera('EPSG:3946', 100, 100);
     camera.camera3D.position.copy(new Coordinates('EPSG:3946', 0, 0, 100).xyz());
     camera.camera3D.updateMatrixWorld(true);
-    const context = { distance: { min: 0, max: 0, update: () => { } }, camera };
 
     it('should compute distance correctly', () => {
         const tileset = tilesetWithBox();
@@ -55,11 +53,9 @@ describe('Distance computation using boundingVolume.box', () => {
         const tile = new Object3D();
         configureTile(tile, { }, tileIndex.index['1']);
 
-        tested.calculateCameraDistance(camera, tile);
+        tested.calculateCameraDistance(camera.camera3D, tile);
 
-        console.log(tile.distance)
-
-        assert.equal(tile.distance, 100 - 1);
+        expect(tile.distance).toEqual({ min: 99, max: 102.46410161513775 });
     });
 
     it('should affected by transform', () => {
@@ -73,11 +69,9 @@ describe('Distance computation using boundingVolume.box', () => {
         const tile = new Object3D();
         configureTile(tile, { }, tileIndex.index['1']);
 
-        tile.updateMatrixWorld(true);
+        tested.calculateCameraDistance(camera.camera3D, tile);
 
-        computeNodeSSE(context, tile);
-
-        assert.equal(tile.distance, 100 - 1 * 0.01 - 10);
+        expect(tile.distance).toEqual({ max: 90.02464101615138, min: 89.99 });
     });
 });
 
@@ -88,7 +82,6 @@ describe('Distance computation using boundingVolume.sphere', () => {
     const camera = new Camera('EPSG:3946', 100, 100);
     camera.camera3D.position.copy(new Coordinates('EPSG:3946', 0, 0, 100).xyz());
     camera.camera3D.updateMatrixWorld(true);
-    const context = { distance: { min: 0, max: 0, update: () => { } }, camera };
 
     it('should compute distance correctly', () => {
         const tileset = tilesetWithSphere();
@@ -97,9 +90,9 @@ describe('Distance computation using boundingVolume.sphere', () => {
         const tile = new Object3D();
         configureTile(tile, { }, tileIndex.index['1']);
 
-        computeNodeSSE(context, tile);
+        tested.calculateCameraDistance(camera.camera3D, tile);
 
-        assert.equal(tile.distance, 100 - 1);
+        expect(tile.distance).toEqual({ max: 101, min: 99 });
     });
 
     it('should affected by transform', () => {
@@ -113,10 +106,12 @@ describe('Distance computation using boundingVolume.sphere', () => {
         const tile = new Object3D();
         configureTile(tile, { }, tileIndex.index['1']);
 
-        tile.updateMatrixWorld(true);
+        tested.calculateCameraDistance(camera.camera3D, tile);
 
-        computeNodeSSE(context, tile);
+        tested.calculateCameraDistance(camera.camera3D, tile);
 
-        assert.equal(tile.distance, 100 - 1 * 0.01 - 10);
+        // floats...
+        expect(tile.distance.min).toBeCloseTo(89.99, 12);
+        expect(tile.distance.max).toBeCloseTo(90.01, 12);
     });
 });
