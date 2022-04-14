6bf1f8c54b238fac26db51de16a71dfb4ad21806
Antoine <antoine@facchini.fr>
2022-04-14 17:04:53 +0200

    Follow to 'Modified spec/gloss shader to match the glTF spec' in Thre.js

--- src/Parser/GLTFLoader.js
+++ src/Parser/GLTFLoader.js
@@ -556,7 +556,7 @@ export default ( function () {
 
 				var lightPhysicalFragmentChunk = [
 					'PhysicalMaterial material;',
-					'material.diffuseColor = diffuseColor.rgb;',
+					'material.diffuseColor = diffuseColor.rgb * ( 1. - max( specularFactor.r, max( specularFactor.g, specularFactor.b ) ) );',
 					'material.specularRoughness = clamp( 1.0 - glossinessFactor, 0.04, 1.0 );',
 					'material.specularColor = specularFactor.rgb;',
 				].join( '\n' );
