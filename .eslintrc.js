2ca1c7832e76ae2105508dfcc4d1e2e7cba51ec3
Sébastien Guimmara <sguimmara@pobox.com>
2022-06-23 11:55:01 +0200

    .eslintrc.js: reformat with eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,13 +8,13 @@ module.exports = {
         ecmaVersion: 2017,
         sourceType: 'module',
         ecmaFeatures: {
-            impliedStrict: true
-        }
+            impliedStrict: true,
+        },
     },
     settings: {
         'import/resolver': {
-            'webpack': {}
-        }
+            webpack: {},
+        },
     },
     env: {
         browser: true,
@@ -24,7 +24,7 @@ module.exports = {
     },
     rules: {
         'no-console': 'off', // let's log cleverly!
-        eqeqeq: ["error", "smart"],
+        eqeqeq: ['error', 'smart'],
         'no-plusplus': 'off',
         'arrow-parens': ['error', 'as-needed'],
         // this option sets a specific tab width for your code
@@ -33,18 +33,14 @@ module.exports = {
             SwitchCase: 1,
             VariableDeclarator: 1,
             outerIIFEBody: 1,
-            // MemberExpression: null,
-            // CallExpression: {
-            // parameters: null,
-            // },
             FunctionDeclaration: {
                 parameters: 1,
-                body: 1
+                body: 1,
             },
             FunctionExpression: {
                 parameters: 1,
-                body: 1
-            }
+                body: 1,
+            },
         }],
         'one-var': ['error', 'never'],
         'valid-jsdoc': ['error', {
@@ -60,22 +56,22 @@ module.exports = {
         // we make heavy use of for loop, and continue is very handy when used correctly
         'no-continue': 'off',
         'no-param-reassign': 'off', // we use param reassign too much with targets
-        'no-use-before-define': ["error", "nofunc"],
+        'no-use-before-define': ['error', 'nofunc'],
         // same as airbnb, but allow for..of, because our babel config doesn't import generators
         'no-restricted-syntax': [
-          'error',
-          {
-            selector: 'ForInStatement',
-            message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
-          },
-          {
-            selector: 'LabeledStatement',
-            message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
-          },
-          {
-            selector: 'WithStatement',
-            message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
-          },
+            'error',
+            {
+                selector: 'ForInStatement',
+                message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+            },
+            {
+                selector: 'LabeledStatement',
+                message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+            },
+            {
+                selector: 'WithStatement',
+                message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+            },
         ],
         // disabling this because it is not yet possible to be subtle enough.
         // For instance, ok:
@@ -98,7 +94,7 @@ module.exports = {
         'no-bitwise': 'off', // we DO manipulate bits often enough, making this irrelevant
         'max-classes-per-file': 'off', // for me, if we export only one, I don't see the wrong here
     },
-    "globals": {
-        "__DEBUG__": false
-    }
-}
+    globals: {
+        __DEBUG__: false,
+    },
+};
