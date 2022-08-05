e193e52feb3c812a0b34b727f893c5f83e1ff7ce
Sébastien Guimmara <sguimmara@pobox.com>
2022-08-05 14:48:27 +0200

    chore(examples): remove bundle

--- examples/webpack/config.mjs
+++ examples/webpack/config.mjs
@@ -26,9 +26,7 @@ export default (env, argv) => {
                 entry[example] = [`./${jsFile}`];
             }
         });
-    entry.giro3d = ['babel-polyfill', 'url-polyfill', 'whatwg-fetch', path.join(baseDir, 'giro3d.js')];
     entry.index = [path.join(baseDir, '..', 'index.js')];
-    entry.debug = [path.join(baseDir, '..', '..', 'utils/debug/Main.js')];
 
     return {
         watchOptions: {
