dd7f72ab0229bf237a0438ca4bbf9ce8c5791d99
Antoine Facchini <antoine.facchini@oslandia.com>
2022-08-09 13:55:54 +0200

    refactor(StaticSource): move code from static provider to source

--- src/sources/StaticSource.js
+++ src/sources/StaticSource.js
@@ -1,3 +1,5 @@
+import Fetcher from '../Provider/Fetcher.js';
+
 /**
  * Class used as a source to create a {@linkcode module:Core/layer/Layer~Layer layer}
  * when the data is a custom tileset.
@@ -13,8 +15,16 @@ class StaticSource {
      * @param {object} options.networkOptions Network options
      */
     constructor(options = {}) {
-        this.url = options.url;
+        this.url = new URL(options.url, window.location);
         this.networkOptions = options.networkOptions;
+    }
+
+    buildUrl(image) {
+        return this.url.href.substr(0, this.url.href.lastIndexOf('/') + 1) + image;
+    }
+
+    fetchMetadata() {
+        return Fetcher.json(this.url.href, this.networkOptions);
     }
 }
 
