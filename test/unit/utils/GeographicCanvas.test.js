7a3609c7b64b3470046f7ad48e9a530b392d565e
Sébastien Guimmara <sguimmara@pobox.com>
2022-09-02 09:57:30 +0200

    fix(OLTileProvider): support arbitrary number of tiles per Map tile
    OpenLayers tile grids almost never match the Map grid.
    
    Most of the time, a Map tile is covered by at least 2 (more generally 4)
    source tiles. However, in the previous implementation, we expected
    1 source tile to cover a Map tile, forcing us to select a lower level
    of detail (Z) to have a single tile.
    
    This degraded visual quality a lot.
    
    In this fix, OLTileProvider is able to process several source tile per
    Map tile, by combining all source tiles into a single texture using a canvas.
    
    A new helper class is introduced : GeographicCanvas.
    
    OLTileProvider
    ==============
    
    `getPossibleTextureImprovements()` is modified to return an OpenLayers'
    TileRange instead of a single tile. This range is selected to provide the
    best level of detail for the destination tile, taking the tile's dimensions
    into account.
    
    Then, `executeCommand()` processes the previously computed TileRange by
    loading all tiles in the range. OpenLayers takes care of caching tiles
    so we don't have to worry about loading twice the same source tile for
    different Map tiles.
    
    Once all tiles are loaded, they are combined into a single image using
    a `GeographicCanvas`.
    
    GeographicCanvas
    ================
    
    This class is a wrapper over a regular HTMLCanvasElement.
    It can draw images in the canvas using the image's extent, and perform
    the transformations into canvas coordinates.
    
    fixes #13

---
+++ test/unit/utils/GeographicCanvas.test.js
@@ -0,0 +1,120 @@
+import GeographicCanvas, { toCanvasNormalizedCoordinates } from '../../../src/utils/GeographicCanvas.js';
+import Extent from '../../../src/Core/Geographic/Extent.js';
+
+describe('GeographicCanvas', () => {
+    function makeMockCanvas(width = 256, height = 256) {
+        const context = {
+            drawImage: jest.fn(),
+        };
+        const result = {
+            width,
+            height,
+            getContext: () => context,
+        };
+        return result;
+    }
+
+    describe('draw', () => {
+        it('should call drawImage with correct values', () => {
+            const bottomLeftQuadrant = new Extent('EPSG:4326', 0, 10, 0, 10);
+            const canvas = new GeographicCanvas(
+                {
+                    canvas: makeMockCanvas(256, 256),
+                    extent: bottomLeftQuadrant,
+                },
+            );
+
+            const image = {};
+            const imageExtent = new Extent('EPSG:4326', 0, 5, 0, 5);
+            canvas.draw(image, imageExtent);
+
+            expect(canvas.context.drawImage)
+                .toHaveBeenCalledWith(image, 0, 128, 128, 128);
+        });
+    });
+
+    describe('toCanvasNormalizedCoordinates', () => {
+        it('should return 0 0 1 1 if both extents are the same', () => {
+            const extent = new Extent('EPSG:4326', 1, 10, 2, 20);
+            const canvas = new GeographicCanvas({ canvas: makeMockCanvas(), extent });
+            const {
+                x, y, w, h,
+            } = toCanvasNormalizedCoordinates(extent, canvas);
+
+            expect(x).toBe(0);
+            expect(y).toBe(0);
+            expect(w).toBe(1);
+            expect(h).toBe(1);
+        });
+
+        it('should return 0 0.5 0.5 0.5 if source is the bottom left quadrant', () => {
+            const source = new Extent('EPSG:4326', 0, 10, 0, 10);
+            const dest = new Extent('EPSG:4326', 0, 20, 0, 20);
+            const canvas = new GeographicCanvas({ canvas: makeMockCanvas(), extent: dest });
+            const {
+                x, y, w, h,
+            } = toCanvasNormalizedCoordinates(source, canvas);
+
+            expect(x).toBe(0);
+            expect(y).toBe(0.5);
+            expect(w).toBe(0.5);
+            expect(h).toBe(0.5);
+        });
+
+        it('should return 0 0 0.5 0.5 if source is the top left quadrant', () => {
+            const source = new Extent('EPSG:4326', 0, 10, 10, 20);
+            const dest = new Extent('EPSG:4326', 0, 20, 0, 20);
+            const canvas = new GeographicCanvas({ canvas: makeMockCanvas(), extent: dest });
+            const {
+                x, y, w, h,
+            } = toCanvasNormalizedCoordinates(source, canvas);
+
+            expect(x).toBe(0);
+            expect(y).toBe(0);
+            expect(w).toBe(0.5);
+            expect(h).toBe(0.5);
+        });
+
+        it('should return 0.5 0.5 0.5 0.5 if source is the bottom right quadrant', () => {
+            const source = new Extent('EPSG:4326', 10, 20, 0, 10);
+            const dest = new Extent('EPSG:4326', 0, 20, 0, 20);
+            const canvas = new GeographicCanvas({ canvas: makeMockCanvas(), extent: dest });
+            const {
+                x, y, w, h,
+            } = toCanvasNormalizedCoordinates(source, canvas);
+
+            expect(x).toBe(0.5);
+            expect(y).toBe(0.5);
+            expect(w).toBe(0.5);
+            expect(h).toBe(0.5);
+        });
+
+        it('should return 0.5 0 0.5 0.5 if source is the top right quadrant', () => {
+            const source = new Extent('EPSG:4326', 10, 20, 10, 20);
+            const dest = new Extent('EPSG:4326', 0, 20, 0, 20);
+            const canvas = new GeographicCanvas({ canvas: makeMockCanvas(), extent: dest });
+            const {
+                x, y, w, h,
+            } = toCanvasNormalizedCoordinates(source, canvas);
+
+            expect(x).toBe(0.5);
+            expect(y).toBe(0);
+            expect(w).toBe(0.5);
+            expect(h).toBe(0.5);
+        });
+
+        it('should return -0.5 -0.5 2 2 if source is twice as big as dest, and centered', () => {
+            const source = new Extent('EPSG:4326', -10, 30, -10, 30);
+            const dest = new Extent('EPSG:4326', 0, 20, 0, 20);
+            const canvas = new GeographicCanvas({ canvas: makeMockCanvas(), extent: dest });
+            const {
+                x, y, w, h,
+            } = toCanvasNormalizedCoordinates(source, canvas);
+
+            expect(x).toBe(-0.5);
+            expect(y).toBe(-0.5);
+            expect(w).toBe(2);
+            expect(h).toBe(2);
+        });
+    });
+});
