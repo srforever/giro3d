bf6195f36be0f141ddc09e2ed5491cc60c936a74
Augustin Trancart <augustin.trancart@oslandia.com>
2023-01-26 21:03:10 +0100

    feat(FeatureCollection): add a new entity to transform feature source into meshes
    Fixes #252

---
+++ test/unit/entities/FeatureCollection.test.js
@@ -0,0 +1,35 @@
+import Extent from '../../../src/core/geographic/Extent.js';
+import FeatureCollection from '../../../src/entities/FeatureCollection.js';
+
+describe('FeatureCollection', () => {
+    describe('constructor', () => {
+        it('should throw on undefined id', () => {
+            expect(() => new FeatureCollection(undefined)).toThrow(/Missing id parameter/);
+        });
+
+        it('should throw if the extent is invalid', () => {
+            // reversed extent (min values are greater than max values)
+            const invalid = new Extent('EPSG:3857', +10, -10, +5, -5);
+
+            expect(() => new FeatureCollection('foo', { extent: invalid })).toThrow(/Invalid extent/);
+        });
+
+        it('should assign the correct options', () => {
+            const extent = new Extent('EPSG:4326', {
+                west: 0, east: 10, south: 0, north: 10,
+            });
+            const fc = new FeatureCollection(
+                'foo',
+                {
+                    extent,
+                    minLevel: 10,
+                    maxLevel: 15,
+                    altitude: 50,
+                },
+            );
+            expect(fc.minLevel).toEqual(10);
+            expect(fc.maxLevel).toEqual(15);
+            expect(fc.extent).toEqual(extent);
+        });
+    });
+});
