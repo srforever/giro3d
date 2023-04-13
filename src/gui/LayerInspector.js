a044c6c76bef4c35dcbd6b07c70165261fcd3e5a
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2023-04-13 10:23:21 +0200

    fix(LayerInspector): fix missing updates

--- src/gui/LayerInspector.js
+++ src/gui/LayerInspector.js
@@ -47,7 +47,7 @@ class LayerInspector extends Panel {
         this.addController(this.layer, 'visible')
             .name('Visible')
             .onChange(() => {
-                this.notify(layer);
+                this.notify(map);
             });
 
         this.addController(this.layer, 'projection')
@@ -77,7 +77,7 @@ class LayerInspector extends Panel {
                 .name('Opacity')
                 .min(0)
                 .max(1)
-                .onChange(() => this.notify(layer));
+                .onChange(() => this.notify(map));
         }
 
         this.extentColor = new Color('#52ff00');
