0ed6456357c3ee78ddcc2f50ae2066451804c84f
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2023-06-20 11:18:03 +0200

    docs(tifftiles): use a custom intersection test
    This example is perfect to illustrate the use of custom intersection
    for image sources. All map tiles that do not overlap the GeoJSON footprint
    are not processed by the layer/source.

--- examples/tifftiles.js
+++ examples/tifftiles.js
@@ -1,3 +1,4 @@
+import * as turf from '@turf/turf';
 import { MapControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import XYZ from 'ol/source/XYZ.js';
 import Extent from '@giro3d/giro3d/core/geographic/Extent.js';
@@ -7,6 +8,7 @@ import Map from '@giro3d/giro3d/entities/Map.js';
 import Inspector from '@giro3d/giro3d/gui/Inspector.js';
 import GeoTIFFFormat from '@giro3d/giro3d/formats/GeoTIFFFormat.js';
 import TiledImageSource from '@giro3d/giro3d/sources/TiledImageSource.js';
+import Fetcher from '@giro3d/giro3d/utils/Fetcher.js';
 
 import StatusBar from './widgets/StatusBar.js';
 
@@ -17,7 +19,7 @@ const y = 5811042.273912458;
 const extent = new Extent(
     'EPSG:3857',
     x - 12000, x + 13000,
-    y - 4000, y + 21000,
+    y - 4000, y + 26000,
 );
 
 // `viewerDiv` will contain giro3d' rendering area (the canvas element)
@@ -43,22 +45,61 @@ const map = new Map('planar', {
 
 instance.add(map);
 
-const source = new TiledImageSource({
-    source: new XYZ({
-        minZoom: 10,
-        maxZoom: 16,
-        url: 'https://3d.oslandia.com/dem/MtStHelens-tiles/{z}/{x}/{y}.tif',
-    }),
-    format: new GeoTIFFFormat(),
-});
+let footprint;
 
-map.addLayer(new ElevationLayer(
-    'osm',
-    {
-        extent,
-        source,
-    },
-)).catch(e => console.error(e));
+/**
+ * A function that will override the default intersection test for image sources (by default
+ * performing intersection on extents, i.e rectangles). Here we want to exclude tiles that do not
+ * intersect with the GeoJSON footprint of the dataset.
+ *
+ * @param {Extent} tileExtent The extent to test.
+ */
+function customIntersectionTest(tileExtent) {
+    if (!footprint) {
+        return true;
+    }
+
+    const corners = [
+        [tileExtent.topLeft().x(), tileExtent.topLeft().y()],
+        [tileExtent.topRight().x(), tileExtent.topRight().y()],
+        [tileExtent.bottomRight().x(), tileExtent.bottomRight().y()],
+        [tileExtent.bottomLeft().x(), tileExtent.bottomLeft().y()],
+    ];
+
+    const extentAsPolygon = turf.helpers.polygon([[
+        corners[0],
+        corners[1],
+        corners[2],
+        corners[3],
+        corners[0],
+    ]]);
+
+    const intersects = turf.booleanIntersects(turf.toWgs84(extentAsPolygon), footprint);
+
+    return intersects;
+}
+
+Fetcher.json('data/MtStHelens-footprint.geojson').then(geojson => {
+    footprint = turf.toWgs84(geojson);
+
+    const source = new TiledImageSource({
+        containsFn: customIntersectionTest, // Here we specify our custom intersection test
+        source: new XYZ({
+            minZoom: 10,
+            maxZoom: 16,
+            url: 'https://3d.oslandia.com/dem/MtStHelens-tiles/{z}/{x}/{y}.tif',
+        }),
+        format: new GeoTIFFFormat(),
+    });
+
+    map.addLayer(new ElevationLayer(
+        'osm',
+        {
+            extent,
+            source,
+        },
+    )).catch(e => console.error(e));
+}).catch(e => console.error(e));
 
 const center = extent.center().xyz();
 instance.camera.camera3D.position.set(center.x, center.y - 1, 50000);
