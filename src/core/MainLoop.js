8f08dedfd6ee81952e10fccca88f6363a5abd5da
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2023-07-04 11:50:46 +0200

    refactor(MainLoop): convert to class

--- src/core/MainLoop.js
+++ src/core/MainLoop.js
@@ -36,28 +36,191 @@ export const MAIN_LOOP_EVENTS = {
     UPDATE_END: 'update_end',
 };
 
-function MainLoop(scheduler, engine, options = {}) {
-    this.renderingState = RENDERING_PAUSED;
-    this.needsRedraw = false;
-    this.scheduler = scheduler;
-    this.gfxEngine = engine; // TODO: remove me
-    this._updateLoopRestarted = true;
-    this.maxFar = options.maxFar || MAX_DISTANCE;
-    this.minNear = options.minNear || MIN_DISTANCE;
-}
-
-MainLoop.prototype = Object.create(EventDispatcher.prototype);
-MainLoop.prototype.constructor = MainLoop;
-
-MainLoop.prototype.scheduleUpdate = function scheduleUpdate(instance, forceRedraw) {
-    this.needsRedraw |= forceRedraw;
-
-    if (this.renderingState !== RENDERING_SCHEDULED) {
-        this.renderingState = RENDERING_SCHEDULED;
-
-        requestAnimationFrame(timestamp => { this._step(instance, timestamp); });
+class MainLoop extends EventDispatcher {
+    constructor(scheduler, engine, options = {}) {
+        super();
+        this.renderingState = RENDERING_PAUSED;
+        this.needsRedraw = false;
+        this.scheduler = scheduler;
+        this.gfxEngine = engine; // TODO: remove me
+        this._updateLoopRestarted = true;
+        this.maxFar = options.maxFar || MAX_DISTANCE;
+        this.minNear = options.minNear || MIN_DISTANCE;
     }
-};
+
+    scheduleUpdate(instance, forceRedraw) {
+        this.needsRedraw |= forceRedraw;
+
+        if (this.renderingState !== RENDERING_SCHEDULED) {
+            this.renderingState = RENDERING_SCHEDULED;
+
+            requestAnimationFrame(timestamp => { this._step(instance, timestamp); });
+        }
+    }
+
+    _update(instance, updateSources, dt) {
+        const context = new Context(instance.camera, this.scheduler, instance);
+
+        // Reset near/far to default value to allow update function to test
+        // visibility using camera's frustum; without depending on the near/far
+        // values which are only used for rendering.
+        instance.camera.camera3D.near = this.minNear;
+        instance.camera.camera3D.far = this.maxFar;
+        // We can't just use camera3D.updateProjectionMatrix() because part of
+        // the update process use camera._viewMatrix, and this matrix depends
+        // on near/far values.
+        instance.camera.update();
+
+        for (const entity of instance.getObjects()) {
+            context.fastUpdateHint = undefined;
+            context.entity = entity;
+            if (entity.ready && entity.visible) {
+                instance.execFrameRequesters(
+                    MAIN_LOOP_EVENTS.BEFORE_LAYER_UPDATE, dt, this._updateLoopRestarted, entity,
+                );
+
+                // Filter updateSources that are relevant for the entity
+                const srcs = filterChangeSources(updateSources, entity);
+                if (srcs.size > 0) {
+                    // if we don't have any element in srcs, it means we don't need to update
+                    // our layer to display it correctly.  but in this case we still need to
+                    // use layer._distance to calculate near / far hence the reset is here,
+                    // and the update of context.distance is outside of this if
+                    entity._distance.min = Infinity;
+                    entity._distance.max = 0;
+                    // `preUpdate` returns an array of elements to update
+                    const elementsToUpdate = entity.preUpdate(context, srcs);
+                    // `update` is called in `updateElements`.
+                    updateElements(context, entity, elementsToUpdate);
+                    // `postUpdate` is called when this geom layer update process is finished
+                    entity.postUpdate(context, updateSources);
+                }
+                if (entity._distance) {
+                    context.distance.min = Math.min(context.distance.min, entity._distance.min);
+                    if (entity._distance.max === Infinity) {
+                        context.distance.max = this.maxFar;
+                    } else {
+                        context.distance.max = Math.max(
+                            context.distance.max, entity._distance.max,
+                        );
+                    }
+                }
+                instance.execFrameRequesters(
+                    MAIN_LOOP_EVENTS.AFTER_LAYER_UPDATE, dt, this._updateLoopRestarted, entity,
+                );
+            }
+        }
+
+        // TODO document the fact Object3D must be added through threeObjects
+        // if they want to influence the near / far planes
+        instance.threeObjects.traverse(o => {
+            if (!o.visible) {
+                return;
+            }
+            if (o.geometry && o.geometry.boundingSphere) {
+                _tmpSphere.copy(o.geometry.boundingSphere);
+                _tmpSphere.applyMatrix4(o.matrixWorld);
+                const d = _tmpSphere.distanceToPoint(context.camera.camera3D.position);
+                context.distance.min = ThreeMath.clamp(d, 0, context.distance.min);
+
+                context.distance.max = Math.max(context.distance.max, d + 2 * _tmpSphere.radius);
+            }
+        });
+
+        let minDistance = context.distance.min;
+        if (instance.camera.camera3D.isPerspective) {
+            // NOTE: if the object responsible of this value of minDistance is near one
+            // end of the field of instance, the near plane must be at near = minDistance *
+            // cos(fov)
+            const cos = Math.cos(ThreeMath.degToRad(instance.camera.camera3D.fov / 2));
+            minDistance *= minDistance * cos;
+        }
+        // clamp it to minNear / maxFar
+        minDistance = minDistance === Infinity
+            ? this.minNear : ThreeMath.clamp(minDistance, this.minNear, this.maxFar);
+        instance.camera.camera3D.near = minDistance;
+
+        const far = context.distance.max === 0
+            ? this.maxFar : ThreeMath.clamp(context.distance.max, minDistance, this.maxFar);
+        instance.camera.camera3D.far = far;
+
+        instance.camera.update();
+    }
+
+    _step(instance, timestamp) {
+        const dt = timestamp - this._lastTimestamp;
+        instance._executeFrameRequestersRemovals();
+
+        instance.execFrameRequesters(MAIN_LOOP_EVENTS.UPDATE_START, dt, this._updateLoopRestarted);
+
+        const willRedraw = this.needsRedraw;
+        this._lastTimestamp = timestamp;
+
+        // Reset internal state before calling _update (so future calls to Instance.notifyChange()
+        // can properly change it)
+        this.needsRedraw = false;
+        this.renderingState = RENDERING_PAUSED;
+        const updateSources = new Set(instance._changeSources);
+        instance._changeSources.clear();
+
+        // update camera
+        const dim = this.gfxEngine.getWindowSize();
+
+        instance.execFrameRequesters(
+            MAIN_LOOP_EVENTS.BEFORE_CAMERA_UPDATE, dt, this._updateLoopRestarted,
+        );
+        instance.camera.update(dim.x, dim.y);
+        instance.execFrameRequesters(
+            MAIN_LOOP_EVENTS.AFTER_CAMERA_UPDATE, dt, this._updateLoopRestarted,
+        );
+
+        // Disable camera's matrix auto update to make sure the camera's
+        // world matrix is never updated mid-update.
+        // Otherwise inconsistencies can appear because object visibility
+        // testing and object drawing could be performed using different
+        // camera matrixWorld.
+        // Note: this is required at least because WEBGLRenderer calls
+        // camera.updateMatrixWorld()
+        const oldAutoUpdate = instance.camera.camera3D.matrixAutoUpdate;
+        instance.camera.camera3D.matrixAutoUpdate = false;
+
+        // update data-structure
+        this._update(instance, updateSources, dt);
+
+        if (this.scheduler.commandsWaitingExecutionCount() === 0) {
+            this.dispatchEvent({ type: 'command-queue-empty' });
+        }
+
+        // Redraw *only* if needed.
+        // (redraws only happen when this.needsRedraw is true, which in turn only happens when
+        // instance.notifyChange() is called with redraw=true)
+        // As such there's no continuous update-loop, instead we use a ad-hoc update/render
+        // mechanism.
+        if (willRedraw) {
+            this._renderInstance(instance, dt);
+        }
+
+        // next time, we'll consider that we've just started the loop if we are still PAUSED now
+        this._updateLoopRestarted = this.renderingState === RENDERING_PAUSED;
+
+        instance.camera.camera3D.matrixAutoUpdate = oldAutoUpdate;
+
+        instance.execFrameRequesters(MAIN_LOOP_EVENTS.UPDATE_END, dt, this._updateLoopRestarted);
+    }
+
+    _renderInstance(instance, dt) {
+        instance.execFrameRequesters(MAIN_LOOP_EVENTS.BEFORE_RENDER, dt, this._updateLoopRestarted);
+
+        if (instance.render) {
+            instance.render();
+        } else {
+            // use default rendering method
+            this.gfxEngine.render(instance.scene, instance.camera.camera3D);
+        }
+
+        instance.execFrameRequesters(MAIN_LOOP_EVENTS.AFTER_RENDER, dt, this._updateLoopRestarted);
+    }
+}
 
 function updateElements(context, entity, elements) {
     if (!elements) {
@@ -118,167 +281,5 @@ function filterChangeSources(updateSources, entity) {
     });
     return fullUpdate ? new Set([entity]) : filtered;
 }
-
-MainLoop.prototype._update = function _update(instance, updateSources, dt) {
-    const context = new Context(instance.camera, this.scheduler, instance);
-
-    // Reset near/far to default value to allow update function to test
-    // visibility using camera's frustum; without depending on the near/far
-    // values which are only used for rendering.
-    instance.camera.camera3D.near = this.minNear;
-    instance.camera.camera3D.far = this.maxFar;
-    // We can't just use camera3D.updateProjectionMatrix() because part of
-    // the update process use camera._viewMatrix, and this matrix depends
-    // on near/far values.
-    instance.camera.update();
-
-    for (const entity of instance.getObjects()) {
-        context.fastUpdateHint = undefined;
-        context.entity = entity;
-        if (entity.ready && entity.visible) {
-            instance.execFrameRequesters(
-                MAIN_LOOP_EVENTS.BEFORE_LAYER_UPDATE, dt, this._updateLoopRestarted, entity,
-            );
-
-            // Filter updateSources that are relevant for the entity
-            const srcs = filterChangeSources(updateSources, entity);
-            if (srcs.size > 0) {
-                // if we don't have any element in srcs, it means we don't need to update our layer
-                // to display it correctly.  but in this case we still need to use layer._distance
-                // to calculate near / far hence the reset is here, and the update of
-                // context.distance is outside of this if
-                entity._distance.min = Infinity;
-                entity._distance.max = 0;
-                // `preUpdate` returns an array of elements to update
-                const elementsToUpdate = entity.preUpdate(context, srcs);
-                // `update` is called in `updateElements`.
-                updateElements(context, entity, elementsToUpdate);
-                // `postUpdate` is called when this geom layer update process is finished
-                entity.postUpdate(context, updateSources);
-            }
-            if (entity._distance) {
-                context.distance.min = Math.min(context.distance.min, entity._distance.min);
-                if (entity._distance.max === Infinity) {
-                    context.distance.max = this.maxFar;
-                } else {
-                    context.distance.max = Math.max(
-                        context.distance.max, entity._distance.max,
-                    );
-                }
-            }
-            instance.execFrameRequesters(
-                MAIN_LOOP_EVENTS.AFTER_LAYER_UPDATE, dt, this._updateLoopRestarted, entity,
-            );
-        }
-    }
-
-    // TODO document the fact Object3D must be added through threeObjects if they want to influence
-    // near / far plane
-    instance.threeObjects.traverse(o => {
-        if (!o.visible) {
-            return;
-        }
-        if (o.geometry && o.geometry.boundingSphere) {
-            _tmpSphere.copy(o.geometry.boundingSphere);
-            _tmpSphere.applyMatrix4(o.matrixWorld);
-            const d = _tmpSphere.distanceToPoint(context.camera.camera3D.position);
-            context.distance.min = ThreeMath.clamp(d, 0, context.distance.min);
-
-            context.distance.max = Math.max(context.distance.max, d + 2 * _tmpSphere.radius);
-        }
-    });
-
-    let minDistance = context.distance.min;
-    if (instance.camera.camera3D.isPerspective) {
-        // NOTE: if the object responsible of this value of minDistance is near one
-        // end of the field of instance, the near plane must be at near = minDistance *
-        // cos(fov)
-        minDistance *= minDistance * Math.cos(ThreeMath.degToRad(instance.camera.camera3D.fov / 2));
-    }
-    // clamp it to minNear / maxFar
-    minDistance = minDistance === Infinity
-        ? this.minNear : ThreeMath.clamp(minDistance, this.minNear, this.maxFar);
-    instance.camera.camera3D.near = minDistance;
-
-    const far = context.distance.max === 0
-        ? this.maxFar : ThreeMath.clamp(context.distance.max, minDistance, this.maxFar);
-    instance.camera.camera3D.far = far;
-
-    instance.camera.update();
-};
-
-MainLoop.prototype._step = function _step(instance, timestamp) {
-    const dt = timestamp - this._lastTimestamp;
-    instance._executeFrameRequestersRemovals();
-
-    instance.execFrameRequesters(MAIN_LOOP_EVENTS.UPDATE_START, dt, this._updateLoopRestarted);
-
-    const willRedraw = this.needsRedraw;
-    this._lastTimestamp = timestamp;
-
-    // Reset internal state before calling _update (so future calls to Instance.notifyChange()
-    // can properly change it)
-    this.needsRedraw = false;
-    this.renderingState = RENDERING_PAUSED;
-    const updateSources = new Set(instance._changeSources);
-    instance._changeSources.clear();
-
-    // update camera
-    const dim = this.gfxEngine.getWindowSize();
-
-    instance.execFrameRequesters(
-        MAIN_LOOP_EVENTS.BEFORE_CAMERA_UPDATE, dt, this._updateLoopRestarted,
-    );
-    instance.camera.update(dim.x, dim.y);
-    instance.execFrameRequesters(
-        MAIN_LOOP_EVENTS.AFTER_CAMERA_UPDATE, dt, this._updateLoopRestarted,
-    );
-
-    // Disable camera's matrix auto update to make sure the camera's
-    // world matrix is never updated mid-update.
-    // Otherwise inconsistencies can appear because object visibility
-    // testing and object drawing could be performed using different
-    // camera matrixWorld.
-    // Note: this is required at least because WEBGLRenderer calls
-    // camera.updateMatrixWorld()
-    const oldAutoUpdate = instance.camera.camera3D.matrixAutoUpdate;
-    instance.camera.camera3D.matrixAutoUpdate = false;
-
-    // update data-structure
-    this._update(instance, updateSources, dt);
-
-    if (this.scheduler.commandsWaitingExecutionCount() === 0) {
-        this.dispatchEvent({ type: 'command-queue-empty' });
-    }
-
-    // Redraw *only* if needed.
-    // (redraws only happen when this.needsRedraw is true, which in turn only happens when
-    // instance.notifyChange() is called with redraw=true)
-    // As such there's no continuous update-loop, instead we use a ad-hoc update/render
-    // mechanism.
-    if (willRedraw) {
-        this._renderInstance(instance, dt);
-    }
-
-    // next time, we'll consider that we've just started the loop if we are still PAUSED now
-    this._updateLoopRestarted = this.renderingState === RENDERING_PAUSED;
-
-    instance.camera.camera3D.matrixAutoUpdate = oldAutoUpdate;
-
-    instance.execFrameRequesters(MAIN_LOOP_EVENTS.UPDATE_END, dt, this._updateLoopRestarted);
-};
-
-MainLoop.prototype._renderInstance = function _renderInstance(instance, dt) {
-    instance.execFrameRequesters(MAIN_LOOP_EVENTS.BEFORE_RENDER, dt, this._updateLoopRestarted);
-
-    if (instance.render) {
-        instance.render();
-    } else {
-        // use default rendering method
-        this.gfxEngine.render(instance.scene, instance.camera.camera3D);
-    }
-
-    instance.execFrameRequesters(MAIN_LOOP_EVENTS.AFTER_RENDER, dt, this._updateLoopRestarted);
-};
 
 export default MainLoop;
