be866c7ce81ea7af868766c252e782772a513a55
Augustin Trancart <augustin.trancart@oslandia.com>
2023-07-26 19:33:35 +0200

    feat(FeatureCollectionInspector): add a button to pick a feature and display only this one

--- src/gui/FeatureCollectionInspector.js
+++ src/gui/FeatureCollectionInspector.js
@@ -17,6 +17,71 @@ function applyToMaterial(root, entity, callback) {
     });
 }
 
+// NOTE: this is a quick and custom implementation of the state pattern. Each state have a
+// `doButtonAction` which implements the state changing when the user click on the button.
+// doButtonAction and enter (called when we switch to this state) receive a `switchState` callback
+// to give them a chance to transitino the state
+const FILTER_STATE = {
+    NONE: {
+        cursor: 'auto',
+        buttonLabel: 'Hide all but clicked',
+        enter: function enter(switchState, context) {
+            const { instance, features } = context;
+            features.object3d.traverse(o => {
+                if (o.material) {
+                    o.material.visible = true;
+                }
+            });
+            instance.notifyChange(true);
+        },
+        doButtonAction: function doButtonAction(switchState) {
+            switchState(FILTER_STATE.PICKING);
+        },
+    },
+    PICKING: {
+        cursor: 'crosshair',
+        buttonLabel: 'Cancel picking',
+        enter: function enter(switchState, context) {
+            const { instance, features } = context;
+            if (!this._filteringFn) {
+                this._filteringFn = e => {
+                    const picked = instance.pickObjectsAt(
+                        e,
+                        { limit: 1, radius: 1, where: [features] },
+                    );
+                    if (picked.length !== 0) {
+                        instance.domElement.removeEventListener('click', this._filteringFn);
+                        switchState(FILTER_STATE.FILTERING, picked[0]);
+                    }
+                };
+            }
+            instance.domElement.addEventListener('click', this._filteringFn);
+        },
+        doButtonAction: function doButtonAction(switchState, { instance }) {
+            instance.domElement.removeEventListener('click', this._filteringFn);
+            switchState(FILTER_STATE.NONE);
+        },
+    },
+    FILTERING: {
+        cursor: 'auto',
+        buttonLabel: 'Cancel hiding',
+        enter: function(switchState, context, picked) {
+            const { instance, features } = context;
+            console.log('Filtering', picked, picked.object);
+            const filteringFeature = picked.object.userData;
+            features.object3d.traverse(o => {
+                if (o.material && o.userData !== filteringFeature) {
+                    o.material.visible = false;
+                }
+            });
+            instance.notifyChange(true);
+        },
+        doButtonAction: function(switchState) {
+            switchState(FILTER_STATE.NONE);
+        },
+    }
+}
+
 class FeatureCollectionInspector extends EntityInspector {
     /**
      * Creates an instance of FeaturesInspector.
@@ -68,10 +133,26 @@ class FeatureCollectionInspector extends EntityInspector {
             .name('Freeze updates')
             .onChange(v => this.toggleFrozen(v));
         this.addController(this, 'dumpTiles').name('Dump tiles in console');
+        this.hideAllController = this.addController(this, 'hideAllButClicked');
+        this.filterState = FILTER_STATE.NONE;
+        this.applyFilterState(this.filterState, this.features);
     }
 
     dumpTiles() {
         console.log(this.features.level0Nodes);
+    }
+
+    applyFilterState(state, ...args) {
+        this.instance.domElement.style.cursor = state.cursor;
+        this.hideAllController.name(state.buttonLabel);
+        this.filterState = state;
+        state.enter(this.applyFilterState.bind(this), { features: this.features, instance:
+            this.instance }, ...args);
+    }
+
+    hideAllButClicked() {
+        this.filterState.doButtonAction(this.applyFilterState.bind(this), { features: this.features,
+            instance: this.instance });
     }
 
     /**
