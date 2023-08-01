e6ff6794c959312110916432cde685f48ff59206
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2023-08-01 11:24:11 +0200

    feat(TextureGenerator): supports no-data for 3 and 4 channel images

--- src/utils/TextureGenerator.js
+++ src/utils/TextureGenerator.js
@@ -25,6 +25,7 @@ import {
     UnsignedShort5551Type,
     ClampToEdgeWrapping,
     LinearFilter,
+    MathUtils,
 } from 'three';
 import Interpretation, { Mode } from '../core/layer/Interpretation.js';
 
@@ -41,8 +42,7 @@ function fillBuffer(buf, options, opaqueValue, ...pixelData) {
 
     if (options.scaling) {
         const { min, max } = options.scaling;
-        const factor = 255 / (max - min);
-        getValue = x => Math.round((x - min) * factor);
+        getValue = x => Math.floor(MathUtils.mapLinear(x, min, max, 0, 255));
     } else {
         getValue = x => x;
     }
@@ -69,30 +69,68 @@ function fillBuffer(buf, options, opaqueValue, ...pixelData) {
         }
     }
     if (pixelData.length === 3) {
-        const r = pixelData[0];
-        const g = pixelData[1];
-        const b = pixelData[2];
-        const length = r.length;
+        const rChannel = pixelData[0];
+        const gChannel = pixelData[1];
+        const bChannel = pixelData[2];
+        const length = rChannel.length;
+        let a;
         for (let i = 0; i < length; i++) {
             const idx = i * 4;
-            buf[idx + 0] = getValue(r[i]);
-            buf[idx + 1] = getValue(g[i]);
-            buf[idx + 2] = getValue(b[i]);
-            buf[idx + 3] = opaqueValue;
+
+            let r = rChannel[i];
+            let g = gChannel[i];
+            let b = bChannel[i];
+
+            if ((Number.isNaN(r) || r === options.nodata)
+                && (Number.isNaN(g) || g === options.nodata)
+                && (Number.isNaN(b) || b === options.nodata)) {
+                r = DEFAULT_NODATA;
+                g = DEFAULT_NODATA;
+                b = DEFAULT_NODATA;
+                a = TRANSPARENT;
+            } else {
+                r = getValue(r);
+                g = getValue(g);
+                b = getValue(b);
+                a = opaqueValue;
+            }
+
+            buf[idx + 0] = r;
+            buf[idx + 1] = g;
+            buf[idx + 2] = b;
+            buf[idx + 3] = a;
         }
     }
     if (pixelData.length === 4) {
-        const r = pixelData[0];
-        const g = pixelData[1];
-        const b = pixelData[2];
-        const a = pixelData[3];
-        const length = r.length;
+        const rChannel = pixelData[0];
+        const gChannel = pixelData[1];
+        const bChannel = pixelData[2];
+        const aChannel = pixelData[3];
+        const length = rChannel.length;
         for (let i = 0; i < length; i++) {
             const idx = i * 4;
-            buf[idx + 0] = getValue(r[i]);
-            buf[idx + 1] = getValue(g[i]);
-            buf[idx + 2] = getValue(b[i]);
-            buf[idx + 3] = getValue(a[i]);
+            let r = rChannel[i];
+            let g = gChannel[i];
+            let b = bChannel[i];
+            let a = aChannel[i];
+
+            if ((Number.isNaN(r) || r === options.nodata)
+                && (Number.isNaN(g) || g === options.nodata)
+                && (Number.isNaN(b) || b === options.nodata)) {
+                r = DEFAULT_NODATA;
+                g = DEFAULT_NODATA;
+                b = DEFAULT_NODATA;
+                a = TRANSPARENT;
+            } else {
+                r = getValue(r);
+                g = getValue(g);
+                b = getValue(b);
+            }
+
+            buf[idx + 0] = r;
+            buf[idx + 1] = g;
+            buf[idx + 2] = b;
+            buf[idx + 3] = a;
         }
     }
     return buf;
