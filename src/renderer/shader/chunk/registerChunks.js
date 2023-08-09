37919730323cf2e8931673f6f885a308d67c3e42
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2023-08-09 16:16:59 +0200

    refactor(c3DEngine): register custom chunks

---
+++ src/renderer/shader/chunk/registerChunks.js
@@ -0,0 +1,9 @@
+import { ShaderChunk } from 'three';
+
+// We use non camel-case file names to be as consistent as possible with three.js naming scheme
+/* eslint-disable camelcase */
+
+export default function registerChunks() {
+
+}
+/* eslint-enable camelcase */
