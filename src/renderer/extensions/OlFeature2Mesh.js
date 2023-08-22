1e955ed7a7dfd0dde277e26e20003f4b0555759c
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2023-08-22 13:56:08 +0200

    fix(OLFeature2Mesh): compute correct normals for buildings

--- src/renderer/extensions/OlFeature2Mesh.js
+++ src/renderer/extensions/OlFeature2Mesh.js
@@ -10,8 +10,19 @@ import {
     Points,
     PointsMaterial,
     Vector3,
+    Plane,
 } from 'three';
 import Earcut from 'earcut';
+import { Feature } from 'ol';
+
+const X = 0;
+const Y = 1;
+const Z = 2;
+
+const tmpv0 = new Vector3(0, 0, 0);
+const tmpv1 = new Vector3(0, 0, 0);
+const tmpv2 = new Vector3(0, 0, 0);
+const tempPlane = new Plane();
 
 function getValue(objOrFn, defaultValue, ...args) {
     if (objOrFn) {
@@ -41,47 +52,6 @@ function fillColorArray(colors, length, r, g, b, offset) {
         colors[3 * i] = r;
         colors[3 * i + 1] = g;
         colors[3 * i + 2] = b;
-    }
-}
-
-/**
- * Add indices for the side faces.
- * We loop over the contour and create a side face made of two triangles.
- *
- * For a ring made of (n) coordinates, there are (n*2) vertices.
- * The (n) first vertices are on the roof, the (n) other vertices are on the floor.
- *
- * If index (i) is on the roof, index (i+length) is on the floor.
- *
- * @param {number[]} indices Array of indices to push to
- * @param {number} length Total vertices count in the geom (excluding the extrusion ones)
- * @param {number} offset the offset in the array
- * @param {number} count the number of indices
- * @param {boolean} isClockWise Wrapping direction
- */
-function addExtrudedPolygonSideFaces(indices, length, offset, count, isClockWise) {
-    // loop over contour length, and for each point of the contour,
-    // add indices to make two triangle, that make the side face
-    for (let i = offset; i < offset + count - 1; ++i) {
-        if (isClockWise) {
-            // first triangle indices
-            indices.push(i);
-            indices.push(i + length);
-            indices.push(i + 1);
-            // second triangle indices
-            indices.push(i + 1);
-            indices.push(i + length);
-            indices.push(i + length + 1);
-        } else {
-            // first triangle indices
-            indices.push(i + length);
-            indices.push(i);
-            indices.push(i + length + 1);
-            // second triangle indices
-            indices.push(i + length + 1);
-            indices.push(i);
-            indices.push(i + 1);
-        }
     }
 }
 
@@ -165,17 +135,165 @@ function featureToPolygon(feature, offset, options) {
     );
 }
 
+/**
+ * @param {number[]} coordinates The coordinate of the closed shape that form the roof.
+ * @param {number} stride The stride in the coordinate array (2 for XY, 3 for XYZ)
+ * @param {number[]} offset The offset to apply to vertex positions.
+ * @param {number} pointCount The point count in the closed shape, without duplication of
+ * the first/last point
+ * @param {number[]|number} altitude The altitude.
+ * @param {number[]} positions The array containing the positions of the vertices.
+ * @param {number[]} normals The array containing the normals of the vertices.
+ */
+function createRoofVertices(
+    coordinates,
+    stride,
+    offset,
+    pointCount,
+    altitude,
+    positions,
+    normals,
+) {
+    // TODO use the exact up vector from the local coordinate
+    // This is irrelevant in a planar coordinate system, though, but for a geographically
+    // correct one, we need to compute the normal of the ellipsoid at this point.
+    // We can use the same up vector for all points to save time.
+    const upVector = new Vector3(0, 0, 1);
+
+    for (let i = 0; i < pointCount; i++) {
+        const idx = i * stride;
+        const x = coordinates[idx + X] - offset[X];
+        const y = coordinates[idx + Y] - offset[Y];
+
+        let z = 0;
+        if (stride === 3) {
+            z = coordinates[idx + Z];
+        } else {
+            z = Array.isArray(altitude) ? altitude[idx] : altitude;
+        }
+        z -= offset[Z];
+
+        positions.push(x, y, z);
+        normals.push(upVector.x, upVector.y, upVector.z);
+    }
+}
+
+/**
+ * @param {number} pointCount The point count in the closed shape, without duplication of
+ * the first/last point
+ * @param {number[]} positions The array containing the positions of the vertices.
+ * @param {number[]} normals The array containing the normals of the vertices.
+ * @param {number[]} indices The index array.
+ * @param {number[]|number} extrude The extrusion distance.
+ */
+function createSideWallVertices(pointCount, positions, normals, indices, extrude) {
+    // Each side is formed by the A, B, C, D vertices, where A is the current coordinate,
+    // and B is the next coordinate (thus the segment AB is one side of the polygon).
+    // C and D are the same points but with a Z offset.
+    // Note that each side has its own vertices, as vertices of sides are not shared with
+    // other sides (i.e duplicated) in order to have faceted normals for each side.
+    const VERT_STRIDE = 3; // 3 elements per vertex position (X, Y, Z)
+    let vertexOffset = 0;
+
+    for (let i = 0; i < pointCount; i++) {
+        const idxA = i * VERT_STRIDE;
+        const idxB = ((i + 1) % pointCount) * VERT_STRIDE;
+
+        const Ax = positions[idxA + X];
+        const Ay = positions[idxA + Y];
+        const Az = positions[idxA + Z];
+
+        const Bx = positions[idxB + X];
+        const By = positions[idxB + Y];
+        const Bz = positions[idxB + Z];
+
+        const zOffset = (Array.isArray(extrude) ? extrude[i] : extrude);
+
+        // +Z top
+        //      A                    B
+        // (Ax, Ay, zMax) ---- (Bx, By, zMax)
+        //      |                    |
+        //      |                    |
+        // (Ax, Ay, zMin) ---- (Bx, By, zMin)
+        //      C                    D
+        // -Z bottom
+
+        positions.push(Ax, Ay, Az); // A
+        positions.push(Bx, By, Bz); // B
+        positions.push(Ax, Ay, Az + zOffset); // C
+        positions.push(Bx, By, Bz + zOffset); // D
+
+        // The normal of this wall is easily computed with a plane.
+        const v0 = tmpv0.set(Ax, Ay, Az);
+        const v1 = tmpv1.set(Bx, By, Bz);
+        const v2 = tmpv2.set(Bx, By, Bz + zOffset);
+        const normal = tempPlane.setFromCoplanarPoints(v0, v1, v2).normal;
+
+        // The four points share the same normal of course as they are coplanar
+        normals.push(normal.x, normal.y, normal.z); // A
+        normals.push(normal.x, normal.y, normal.z); // B
+        normals.push(normal.x, normal.y, normal.z); // C
+        normals.push(normal.x, normal.y, normal.z); // D
+
+        // The indices of the side are the following
+        // [A, B, C, C, B, D] to form the two triangles.
+
+        const A = 0;
+        const B = 1;
+        const C = 2;
+        const D = 3;
+
+        const idx = pointCount + vertexOffset;
+
+        indices.push(idx + A);
+        indices.push(idx + B);
+        indices.push(idx + C);
+
+        indices.push(idx + C);
+        indices.push(idx + B);
+        indices.push(idx + D);
+
+        vertexOffset += 4;
+    }
+}
+
+/**
+ * @param {Feature} feature The OL feature.
+ * @param {number} offset TODO
+ * @param {object} options TODO
+ */
 function featureToMultiPolygon(feature, offset, options) {
-    const { altitude, style, extrude } = options;
+    const { altitude, extrude, material } = options;
+
     const geom = feature.getGeometry();
+    /** @type {number} */
+    const stride = geom.stride;
+    /** @type {number[]} */
+    const coordinates = geom.flatCoordinates;
 
-    const threeGeom = prepareBufferGeometry(geom, style.color, altitude, offset, extrude);
+    const bufferGeom = new BufferGeometry();
 
+    // We remove the last point because it is the first point duplicated to close the loop.
+    const pointCount = (coordinates.length / stride) - 1;
+
+    const positions = [];
+    const normals = [];
+
+    // First we compute the positions and normals of the top vertices (that make the "rooftop").
+    createRoofVertices(
+        coordinates,
+        stride,
+        offset,
+        pointCount,
+        altitude,
+        positions,
+        normals,
+    );
+
+    // Then compute the indices of the rooftop by triangulating using the earcut algorithm.
     let indices = [];
     let start = 0;
-    const numVertices = geom.flatCoordinates.length / geom.stride;
     const mapTriangle = i => i + start;
-    const mapTriangleExtrude = i => i + start + numVertices;
     const normalizingEndsFn = end => end / geom.stride - start;
     // we could use geom.getPolygons, but as we already got all the coordinates
     // in one buffer, it's easier to stay at the geom level and use endss
@@ -186,28 +304,29 @@ function featureToMultiPolygon(feature, offset, options) {
         // Convert ends from array element indices to number of 3d coordinates relative to the start
         // of this polygon
         const normalizedEnds = ends.map(normalizingEndsFn);
-        // TODO check slice : supposed to remove the last element because ol (?) close polygons
         const triangles = Earcut(
-            threeGeom.attributes.position.array.slice(start * 3, (polyNormEnd - 1) * 3),
+            positions,
             normalizedEnds.slice(0, -1),
             3,
         );
 
         // shift them to represent their position in the global array
         indices = indices.concat(triangles.map(mapTriangle));
-        if (extrude) {
-            indices = indices.concat(triangles.map(mapTriangleExtrude));
-            // TODO isClockwise?
-            addExtrudedPolygonSideFaces(indices, numVertices, start, polyNormEnd - start, true);
-        }
         start = polyNormEnd;
     }
 
-    threeGeom.setIndex(new BufferAttribute(new Uint16Array(indices), 1));
-    return new Mesh(
-        threeGeom,
-        (options.material ? options.material.clone() : new MeshBasicMaterial()),
-    );
+    // Then we compute the vertical sides of the "building".
+    if (extrude) {
+        createSideWallVertices(pointCount, positions, normals, indices, extrude);
+    }
+
+    bufferGeom.setAttribute('position', new BufferAttribute(new Float32Array(positions), 3));
+    bufferGeom.setAttribute('normal', new BufferAttribute(new Float32Array(normals), 3, true));
+
+    bufferGeom.setIndex(new BufferAttribute(new Uint16Array(indices), 1));
+
+    const mat = (material ? material.clone() : new MeshBasicMaterial());
+    return new Mesh(bufferGeom, mat);
 }
 
 /**
@@ -252,7 +371,7 @@ function featureToMesh(feature, offset, options) {
         default:
     }
 
-    mesh.geometry.computeVertexNormals();
+    // mesh.geometry.computeVertexNormals();
     // set mesh material
     // mesh.material.vertexColors = true;
     // configure mesh material
