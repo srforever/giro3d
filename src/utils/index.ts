87d8c1c36411d7bf6cb42b284348c2a7ca103d1d
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2023-10-12 16:53:07 +0200

    refactor(API): remove TextureGenerator from exposed API

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,11 @@
 import Fetcher from './Fetcher';
 import HttpConfiguration from './HttpConfiguration';
-import TextureGenerator from './TextureGenerator';
 import PromiseUtils from './PromiseUtils';
 import OpenLayersUtils from './OpenLayersUtils';
 
 export {
     Fetcher,
     HttpConfiguration,
-    TextureGenerator,
     PromiseUtils,
     OpenLayersUtils,
 };
