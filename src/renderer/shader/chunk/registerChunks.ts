76afe166890d1f70528d9a1a95c37bcfe10d6492
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2024-03-19 10:57:59 +0100

    refactor(registerChunks): add typing for Giro3D chunks

--- src/renderer/shader/chunk/registerChunks.ts
+++ src/renderer/shader/chunk/registerChunks.ts
@@ -13,8 +13,21 @@ import giro3d_contour_line_pars_fragment from './giro3d_contour_line_pars_fragme
 import giro3d_contour_line_fragment from './giro3d_contour_line_fragment.glsl';
 import giro3d_fragment_shader_header from './giro3d_fragment_shader_header.glsl';
 
+type Giro3DShaderChunk = typeof ShaderChunk & {
+    giro3d_common: string;
+    giro3d_outline_pars_fragment: string;
+    giro3d_outline_fragment: string;
+    giro3d_precision_qualifiers: string;
+    giro3d_compose_layers_fragment: string;
+    giro3d_compose_layers_pars_fragment: string;
+    giro3d_colormap_pars_fragment: string;
+    giro3d_contour_line_pars_fragment: string;
+    giro3d_contour_line_fragment: string;
+    giro3d_fragment_shader_header: string;
+};
+
 export default function registerChunks() {
-    const Giro3dShaderChunk = ShaderChunk as any;
+    const Giro3dShaderChunk = ShaderChunk as Giro3DShaderChunk;
     Giro3dShaderChunk.giro3d_precision_qualifiers = giro3d_precision_qualifiers;
     Giro3dShaderChunk.giro3d_common = giro3d_common;
     Giro3dShaderChunk.giro3d_outline_pars_fragment = giro3d_outline_pars_fragment;
