903f5a1f7870d78868c55967b6afa967e1fb92ae
Sébastien Guimmara <sguimmara@pobox.com>
2024-04-06 19:52:43 +0200

    fix(CogSource): ensure that adjustExtentAndPixelSize() returns integer width/height

--- src/core/layer/Layer.ts
+++ src/core/layer/Layer.ts
@@ -1021,8 +1021,8 @@ abstract class Layer<
                 node,
                 extent,
                 pitch,
-                width,
-                height,
+                width: Math.round(width),
+                height: Math.round(height),
                 geometryExtent: originalExtent,
             });
             this._targets.set(node.id, target);
