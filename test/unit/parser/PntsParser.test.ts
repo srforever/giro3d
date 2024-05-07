775b4090e2858352dfd80baab05d73e7657b40b8
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2024-05-07 08:45:37 +0200

    test(PntsParser.test.js): migrate to TypeScript

--- test/unit/parser/PntsParser.test.ts
+++ test/unit/parser/PntsParser.test.ts
@@ -1,8 +1,8 @@
 import '../setup.js';
 import assert from 'assert';
-import PntsParser from '../../../src/parser/PntsParser';
+import PntsParser from 'src/parser/PntsParser';
 
-function bufferFromString(pnts, size) {
+function bufferFromString(pnts: string, size: number) {
     const buffer = new ArrayBuffer(size);
     const typed = new Uint8Array(buffer);
     let next = 0;
@@ -17,17 +17,18 @@ function bufferFromString(pnts, size) {
 
 describe('PntsParser', () => {
     describe('parse', () => {
-        const pnts = `
-        70 6e 74 73 01 00 00 00  82 00 00 00 48 00 00 00
-        1e 00 00 00 00 00 00 00  00 00 00 00 7b 22 50 4f
-        49 4e 54 53 5f 4c 45 4e  47 54 48 22 3a 32 2c 22
-        50 4f 53 49 54 49 4f 4e  22 3a 7b 22 62 79 74 65
-        4f 66 66 73 65 74 22 3a  30 7d 2c 22 52 47 42 22
-        3a 7b 22 62 79 74 65 4f  66 66 73 65 74 22 3a 32
-        34 7d 7d 20 f1 34 09 42  90 a9 56 42 9c 15 24 41
-        58 9b 0d 42 90 a9 56 42  9c 15 24 41 e0 9b 85 b7
-        8f 89`;
         it('should return the correct points', done => {
+            const pnts = `
+            70 6e 74 73 01 00 00 00  82 00 00 00 48 00 00 00
+            1e 00 00 00 00 00 00 00  00 00 00 00 7b 22 50 4f
+            49 4e 54 53 5f 4c 45 4e  47 54 48 22 3a 32 2c 22
+            50 4f 53 49 54 49 4f 4e  22 3a 7b 22 62 79 74 65
+            4f 66 66 73 65 74 22 3a  30 7d 2c 22 52 47 42 22
+            3a 7b 22 62 79 74 65 4f  66 66 73 65 74 22 3a 32
+            34 7d 7d 20 f1 34 09 42  90 a9 56 42 9c 15 24 41
+            58 9b 0d 42 90 a9 56 42  9c 15 24 41 e0 9b 85 b7
+            8f 89`;
+
             const buffer = bufferFromString(pnts, 16 * 8 + 2);
 
             PntsParser.parse(buffer).then(result => {
