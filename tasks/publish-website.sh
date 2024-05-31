4054f536673566adbe05eee245c0c4ad4e9cf154
Thomas Muguet <thomas.muguet@oslandia.com>
2024-05-31 13:16:57 +0200

    feat(scripts): add dry-run as default behavior for publishing website

--- tasks/publish-website.sh
+++ tasks/publish-website.sh
@@ -3,10 +3,18 @@
 set -e
 
 HERE=$(dirname "$0")
-
 thisCommit=$(git describe --tags --always)
 thisUsername=$(git --no-pager log --format=format:'%an' -n 1)
 thisUseremail=$(git --no-pager log --format=format:'%ae' -n 1)
+
+push=0
+for arg in "$@"
+do
+    if [ "$arg" = "--push" ]; then
+        push=1
+        break
+    fi
+done
 
 [ ! -d "${HERE}/giro3d-website" ] && git clone "https://giro3d:${GIRO3D_ORG_KEY}@gitlab.com/giro3d/giro3d-website.git" "${HERE}/giro3d-website"
 
@@ -24,5 +32,12 @@ if [ -n "$(git status --porcelain)" ]; then
     git config user.email "${thisUseremail}"
     git add .
     git commit -m "Website updates ${thisCommit}"
-    git push origin main
+    if [ $push -eq 1 ]; then
+        echo "Pushing to repo..."
+        git push origin main
+    else
+        echo "Dry-run git push, skipping"
+    fi
+else
+    echo "Nothing new to push, skipping"
 fi
