c476a5dbd147b398edf9f75a96a8be1e94d2840a
Thomas Muguet <thomas.muguet@oslandia.com>
2024-05-31 13:05:47 +0200

    fix(ci): use sh instead of bash for publishing
    Bash is not available on node docker image

--- tasks/publish-website.sh
+++ tasks/publish-website.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -e
 
@@ -8,11 +8,11 @@ thisCommit=$(git describe --tags --always)
 thisUsername=$(git --no-pager log --format=format:'%an' -n 1)
 thisUseremail=$(git --no-pager log --format=format:'%ae' -n 1)
 
-[[ ! -d "${HERE}/giro3d-website" ]] && git clone "https://giro3d:${GIRO3D_ORG_KEY}@gitlab.com/giro3d/giro3d-website.git" "${HERE}/giro3d-website"
+[ ! -d "${HERE}/giro3d-website" ] && git clone "https://giro3d:${GIRO3D_ORG_KEY}@gitlab.com/giro3d/giro3d-website.git" "${HERE}/giro3d-website"
 
 # Clean previous version-specific content
 rm -rf ${HERE}/giro3d-website/dist/next
-[[ -d "${HERE}/../build/site/latest" ]] && rm -rf ${HERE}/giro3d-website/dist/latest
+[ -d "${HERE}/../build/site/latest" ] && rm -rf ${HERE}/giro3d-website/dist/latest
 
 # Copy new website
 cp -r ${HERE}/../build/site/* ${HERE}/giro3d-website/dist/
