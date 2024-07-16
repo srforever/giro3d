96c66b48ccc2d6fe52e7eb869ea58d168f9baa31
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2024-07-16 16:22:47 +0200

    refactor(giro3d_contour_line_fragment.glsl): support arbitrary normals (i.e globe)

--- src/renderer/shader/chunk/giro3d_contour_line_fragment.glsl
+++ src/renderer/shader/chunk/giro3d_contour_line_fragment.glsl
@@ -3,12 +3,9 @@
     // Note: we use the 'height' variable rather than wPosition.z because we want
     // this feature to work event when terrain deformation is disabled, and height
     // is always available.
-    vec3 fdx = vec3( dFdx( wPosition.x ), dFdx( wPosition.y ), dFdx( height ) );
-    vec3 fdy = vec3( dFdy( wPosition.x ), dFdy( wPosition.y ), dFdy( height ) );
-    vec3 worldNormal = normalize( cross( fdx, fdy ) );
 
     // thickness scale
-    float upwardness = dot( worldNormal, vec3( 0.0, 1.0, 0.0 ) );
+    float upwardness = dot( wNormal, vec3( 0.0, 1.0, 0.0 ) );
     float yInv = clamp( 1.0 - abs( upwardness ), 0.0, 1.0 );
     float thicknessScale = pow( yInv, 0.4 );
     thicknessScale *= 0.25 + 0.5 * ( vViewPosition.z + 1.0 ) / 2.0;
