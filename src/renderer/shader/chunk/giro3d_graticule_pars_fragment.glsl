7ad3e380714942920b5d7b11f455cca86ff6ad07
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2024-07-16 15:56:34 +0200

    fix(giro3d_graticule_pars_fragment.glsl): add smooth edges/antialiasing for graticule lines

--- src/renderer/shader/chunk/giro3d_graticule_pars_fragment.glsl
+++ src/renderer/shader/chunk/giro3d_graticule_pars_fragment.glsl
@@ -8,6 +8,29 @@ struct Graticule {
 
 uniform Graticule graticule;
 
+float getGraticuleOpacity(float coordinate, float offset, float step, float thickness) {
+    float dist = mod(coordinate + offset, step);
+
+    float halfThickness = graticule.thickness / 2.0;
+    float falloffWidth = graticule.thickness / 10.0;
+    float fallofStart = halfThickness - falloffWidth;
+
+    if (dist <= halfThickness) {
+        float opacity = 1.0;
+        if (dist > fallofStart) {
+            float normalizedBorderDistance = 1.0 - ((dist - fallofStart) / falloffWidth);
+            opacity *= normalizedBorderDistance;
+        } else if (dist <= falloffWidth) {
+            float normalizedBorderDistance = 1.0 - ((falloffWidth - dist) / falloffWidth);
+            opacity *= normalizedBorderDistance;
+        }
+
+        return opacity;
+    }
+
+    return 0.0;
+}
+
 void drawGraticule(vec2 coordinate, Graticule graticule) {
     vec4 pos = graticule.position;
     float xOffset = pos[0];
@@ -16,17 +39,13 @@ void drawGraticule(vec2 coordinate, Graticule graticule) {
     float yStep = pos[3];
 
     if (xStep > 0. && yStep > 0.) {
-        float xDist = mod(coordinate.x + xOffset, xStep);
-        float yDist = mod(coordinate.y + yOffset, yStep);
+        float xOpacity = getGraticuleOpacity(coordinate.x, xOffset, xStep, graticule.thickness);
+        float yOpacity = getGraticuleOpacity(coordinate.y, yOffset, yStep, graticule.thickness);
 
-        float halfThickness = graticule.thickness / 2.0;
-        float dist = abs(min(xDist, yDist));
+        float opacity = graticule.color.a * max(xOpacity, yOpacity);
 
-        if (dist <= halfThickness) {
-            float opacity = graticule.color.a * smoothstep(0., 1., halfThickness - dist);
-            vec4 finalColor = vec4(graticule.color.rgb, opacity);
-            gl_FragColor = blend(finalColor, gl_FragColor);
-        }
+        vec4 finalColor = vec4(graticule.color.rgb, opacity);
+        gl_FragColor = blend(finalColor, gl_FragColor);
     }
 }
 #endif
