2ebbd694526eebc5c1ea3167a7e1d665d80d916a
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2024-07-17 14:06:53 +0200

    feat(Map): make outline color configurable

--- src/renderer/shader/chunk/giro3d_outline_fragment.glsl
+++ src/renderer/shader/chunk/giro3d_outline_fragment.glsl
@@ -1,11 +1,11 @@
 #if defined(ENABLE_OUTLINES)
 if (vUv.x < OUTLINE_THICKNESS) { // WEST
-    gl_FragColor = OUTLINE_RED;
+    gl_FragColor.rgb = tileOutlineColor;
 } else if (vUv.x > 1.0 - OUTLINE_THICKNESS) { // EAST
-    gl_FragColor = OUTLINE_GREEN;
+    gl_FragColor.rgb = tileOutlineColor;
 } else if (vUv.y < OUTLINE_THICKNESS) { // NORTH
-    gl_FragColor = OUTLINE_BLUE;
+    gl_FragColor.rgb = tileOutlineColor;
 } else if (vUv.y > 1.0 - OUTLINE_THICKNESS) { // SOUTH
-    gl_FragColor = OUTLINE_YELLOW;
+    gl_FragColor.rgb = tileOutlineColor;
 }
 #endif
