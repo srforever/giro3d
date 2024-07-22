df53a9f0ab20a13ad43ec09a86d170f9ce41abf0
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2024-07-22 16:15:01 +0200

    refactor(StatusBar): don't throw error if target does not exist

--- examples/widgets/StatusBar.js
+++ examples/widgets/StatusBar.js
@@ -39,7 +39,7 @@ function processUrl(instance, url) {
             const [x, y, z, tx, ty, tz] = pov.split(',').map(s => Number.parseFloat(s));
 
             instance.camera.camera3D.position.set(x, y, z);
-            instance.controls.target.set(tx, ty, tz);
+            instance.controls?.target?.set(tx, ty, tz);
         } finally {
             instance.notifyChange();
         }
