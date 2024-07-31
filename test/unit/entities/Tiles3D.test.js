4f6a1b7712b2c3497674c5d938ead2f4df2ec368
Augustin Trancart <augustin.trancart@oslandia.com>
2024-07-31 12:11:50 +0200

    fix(Tiles3D): take into account each mesh opacity

--- test/unit/entities/Tiles3D.test.js
+++ test/unit/entities/Tiles3D.test.js
@@ -1,7 +1,7 @@
 import '../setup.js';
 import assert from 'assert';
 import proj4 from 'proj4';
-import { Group, Matrix4, Mesh } from 'three';
+import { Group, Matrix4, Mesh, MeshBasicMaterial } from 'three';
 import Tiles3D from '../../../src/entities/Tiles3D';
 import $3dTilesIndex from '../../../src/entities/3dtiles/3dTilesIndex';
 import Tile from '../../../src/entities/3dtiles/Tile';
@@ -159,6 +159,124 @@ describe('Tiles3D', () => {
             const result = tiles3D.getObjectToUpdateForAttachedLayers(tile);
             assert.ok(Array.isArray(result.elements));
             assert.ok(result.elements.length, 3);
+        });
+    });
+
+    describe('updateOpacity', () => {
+        it('should use this.opacity if set', () => {
+            const material = new MeshBasicMaterial();
+            const entity = new Tiles3D('foo', defaultSource, { material });
+
+            const o = new Group();
+            o.add(new Mesh());
+            o.add(new Mesh());
+            o.add(new Mesh());
+            entity.object3d.add(o);
+
+            for (const mesh of o.children) {
+                expect(mesh.material.opacity).toBe(1);
+                expect(mesh.material.transparent).toBe(false);
+            }
+            entity.opacity = 0.7;
+            expect(entity.material.opacity).toBe(0.7);
+            for (const mesh of o.children) {
+                expect(mesh.material.opacity).toBe(0.7);
+                expect(mesh.material.transparent).toBe(true);
+            }
+        });
+
+        it("should honor object's original opacity if set when there is no material at the entity level", () => {
+            const entity = new Tiles3D('foo', defaultSource);
+
+            const o = new Group();
+            const o1 = new Mesh();
+            o1.material.opacity = 1;
+            o.add(o1);
+            const o2 = new Mesh();
+            o2.material.opacity = 0.1;
+            o.add(o2);
+            const o3 = new Mesh();
+            o3.material.opacity = 0.9;
+            o.add(o3);
+            entity.object3d.add(o);
+            entity.onObjectCreated(o);
+
+            entity.opacity = 0.7;
+            expect(o1.material.opacity).toBeCloseTo(0.7, 5);
+            expect(o1.material.transparent).toBe(true);
+            expect(o2.material.opacity).toBeCloseTo(0.07, 5);
+            expect(o2.material.transparent).toBe(true);
+            expect(o3.material.opacity).toBeCloseTo(0.63, 5);
+            expect(o3.material.transparent).toBe(true);
+        });
+    });
+
+    describe('onObjectCreated', () => {
+        it('should set the opacity of the created object and its descendants to the current opacity value when they have no original opacity', () => {
+            const entity = new Tiles3D('foo', defaultSource);
+
+            const o = new Group();
+            o.add(new Mesh());
+            o.add(new Mesh());
+            o.add(new Mesh());
+            entity.onObjectCreated(o);
+            for (const mesh of o.children) {
+                expect(mesh.material.opacity).toBe(1);
+                expect(mesh.material.transparent).toBe(false);
+            }
+
+            entity.opacity = 0.7;
+            entity.onObjectCreated(o);
+            for (const mesh of o.children) {
+                expect(mesh.material.opacity).toBe(0.7);
+                expect(mesh.material.transparent).toBe(true);
+            }
+        });
+
+        it('should correctly set the opacity of the created object and its descendants when they come with their own opacity', () => {
+            const entity = new Tiles3D('foo', defaultSource);
+
+            const o = new Group();
+            const o1 = new Mesh();
+            o.add(o1);
+            const o2 = new Mesh();
+            o2.material.opacity = 0.1;
+            o.add(o2);
+            const o3 = new Mesh();
+            o3.material.opacity = 0.9;
+            o.add(o3);
+
+            entity.onObjectCreated(o);
+
+            expect(o1.material.opacity).toBe(1);
+            expect(o1.material.transparent).toBe(false);
+            expect(o2.material.opacity).toBe(0.1);
+            expect(o2.material.transparent).toBe(true);
+            expect(o3.material.opacity).toBe(0.9);
+            expect(o3.material.transparent).toBe(true);
+        });
+
+        it('should correctly set the opacity of the created object and its descendants when they come with their own opacity and there is an opacity setup at the entity level', () => {
+            const entity = new Tiles3D('foo', defaultSource);
+
+            const o = new Group();
+            const o1 = new Mesh();
+            o.add(o1);
+            const o2 = new Mesh();
+            o2.material.opacity = 0.1;
+            o.add(o2);
+            const o3 = new Mesh();
+            o3.material.opacity = 0.9;
+            o.add(o3);
+
+            entity.opacity = 0.5;
+            entity.onObjectCreated(o);
+            expect(o1.material.opacity).toBe(0.5);
+            expect(o1.material.transparent).toBe(true);
+            expect(o2.material.opacity).toBe(0.05);
+            expect(o2.material.transparent).toBe(true);
+            expect(o3.material.opacity).toBe(0.45);
+            expect(o3.material.transparent).toBe(true);
         });
     });
 });
